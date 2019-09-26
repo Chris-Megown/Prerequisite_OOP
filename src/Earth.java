8cf8931daa9c70d4406f16b0cc2c73f266b81cd9
Chris Megown <chris.a.megown@gmail.com>
2019-09-26 11:48:39 -0400

    more basics

--- src/Earth.java
+++ src/Earth.java
@@ -1,8 +1,22 @@
 public class Earth {
 
     public static void main(String[] args) {
+
+        //Instance variable
         Human tom;
 
         tom = new Human();
+        tom.name = "Tom";
+        tom.age = 5;
+        tom.heightInInches = 36;
+        tom.eyeColor = "Brown";
+
+        tom.speak();
+
+        Human human1 = new Human("Joe", 25, 72, "Blue");
+        human1.speak();
+
+
+
     }
 }
--- src/Human.java
+++ src/Human.java
@@ -5,23 +5,62 @@ public class Human {
     int heightInInches;
     String eyeColor;
 
+    public Human(String name, int age, int heightInInches, String eyeColor) {
+        this.name = name;
+        this.age = age;
+        this.heightInInches = heightInInches;
+        this.eyeColor = eyeColor;
+    }
+
     public Human(){
 
     }
+
     public void speak() {
         System.out.println("Hello, my name is  " + name);
         System.out.println("I am " + heightInInches + " inches tall");
         System.out.println("I am " + age + " years old");
-        System.out.println("My eye color is " + eyeColor);
+        System.out.println("My eye color is " + eyeColor + "\n");
     }
-        public void eat(){
+
+    public void eat(){
             System.out.println("eating...");
         }
-        public void walk(){
+    public void walk(){
             System.out.println("walking...");
         }
-        public void work(){
+    public void work(){
             System.out.println("working...");
         }
+    public String getName() {
+        return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeightInInches() {
+        return heightInInches;
+    }
+
+    public void setHeightInInches(int heightInInches) {
+        this.heightInInches = heightInInches;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+}
 
